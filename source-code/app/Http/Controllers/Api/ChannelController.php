5c2ac387499788f7674d88948a1b111f3a3715f3
Barba10s <kosta9019@mail.ru>
2026-04-14 11:37:45 +0300

    #1 #change #ChannelController - move channel controller to api directory

--- source-code/app/Http/Controllers/Api/ChannelController.php
+++ source-code/app/Http/Controllers/Api/ChannelController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateChannelRequest;
 use App\Http\Resources\ChannelResource;
 use Illuminate\Http\JsonResponse;
