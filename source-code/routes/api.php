c6a9b35239b5436bcb707428318e1d9dc4c70f4f
Barba10s <kosta9019@mail.ru>
2026-04-14 14:04:54 +0300

    #1 #add #api - add prefix to all routes

--- source-code/routes/api.php
+++ source-code/routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ChannelController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('v1')->group(function () {
-    Route::post('/channels', [ChannelController::class, 'create']);
+    Route::prefix('authorization')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        Route::post('/channels', [ChannelController::class, 'create']);
+    });
 });
