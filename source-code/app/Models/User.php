9deefc2f56325c73091422acc20eaa7c63d797a3
Barba10s <kosta9019@mail.ru>
2026-04-14 14:04:30 +0300

    #1 #add #User - add email attribute

--- source-code/app/Models/User.php
+++ source-code/app/Models/User.php
@@ -6,18 +6,20 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
@@ -44,4 +46,10 @@ class User extends Authenticatable
             ->withTimestamps();
     }
 
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => strtolower(trim($value)),
+        );
+    }
 }
