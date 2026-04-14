571b370928ada90f7f4339b6045ce0fe1700185c
Barba10s <kosta9019@mail.ru>
2026-04-14 11:54:59 +0300

    #1 #add #AuthController - create auth controller with register, login and logout functions

---
+++ source-code/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Authorization\LoginRequest;
+use App\Http\Requests\Authorization\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "success" => true,
+            "token" => $token
+        ], 201);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "success" => false,
+                "message" => "The provided password is not matched."
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "success" => true,
+            "token" => $token
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => 'Logged out'
+        ]);
+    }
+}
