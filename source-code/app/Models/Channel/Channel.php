a31675650bc7c22004445c5b3f490aa91a7ff359
Barba10s <kosta9019@mail.ru>
2026-04-17 15:18:36 +0300

    #2 #change #Models - move channel to channel directory

--- source-code/app/Models/Channel/Channel.php
+++ source-code/app/Models/Channel/Channel.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Channel;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
