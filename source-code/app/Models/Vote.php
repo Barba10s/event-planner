4ba9ef66309ca92c91d2c1660abb3057b730ffff
Barba10s <kosta9019@mail.ru>
2026-04-18 12:42:26 +0300

    #4 #add #Vote - create vote model

---
+++ source-code/app/Models/Vote.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'poll_id',
+        'user_id',
+        'option_id',
+    ];
+
+    protected $casts = [
+        'voted_at' => 'datetime',
+    ];
+
+    public function poll(): BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function option(): BelongsTo
+    {
+        return $this->belongsTo(PollOption::class);
+    }
+}
