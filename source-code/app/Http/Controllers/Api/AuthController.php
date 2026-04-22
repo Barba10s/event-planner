ffe70ae745482e83b2a3bfecae5f775a6ac8d206
Barba10s <kosta9019@mail.ru>
2026-04-22 15:45:05 +0300

    #5 #change #AuthController - change error message

--- source-code/app/Http/Controllers/Api/AuthController.php
+++ source-code/app/Http/Controllers/Api/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
                 "success" => false,
-                "message" => "The provided password is not matched."
+                "message" => "Invalid credentials"
             ], 401);
         }
 
